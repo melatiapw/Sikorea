47caa1dd2d321d764ec1d68f2f777f4f8231b2d4
Melati Aulia Putri Winanto <28047357+melatiapw@users.noreply.github.com>
2018-11-20 15:15:26 +0700

    Update DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
          $this->call(formSeeder::class);
-=======
-        // $this->call(UsersTableSeeder::class);
->>>>>>> 1773a01b08fbf67996392c856e96119bc51f8e76
     }
 }
