9bee3151f77ba2a60cb13855fe498d736bd9cb85
kaniasfr <kaniaekasa@gmail.com>
2018-12-11 20:20:12 +0700

    status db

---
+++ database/seeds/StatusSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('status')->insert([
+                [
+                  'id' =>"3",
+                  'subject' => "Belum Bayar",
+                ],
+                [
+                  'id' =>"4",
+                  'subject' => "Dalam Proses",
+                ],
+                [
+                  'id' =>"5",
+                  'subject' => "Selesai",
+                ],
+
+      ]);
+    }
+}
