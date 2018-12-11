113c96c984a0253394cba8889cb08d87139945a8
kaniasfr <kaniaekasa@gmail.com>
2018-12-11 18:03:04 +0700

    Status Transaksi

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// User
+Route::get('/', 'ControllerHome@index');
+Route::get('/contact', 'ControllerContact@index');
+Route::get('/order', 'ControllerOrder@index');
+Route::get('/cart', 'ControllerCart@index');
+Route::get('/checkout-alamat', 'ControllerCheckout@alamat');
+Route::get('/checkout-bank', 'ControllerCheckout@bank');
+Route::get('/statustransaksi', 'ControllerCheckout@statustransaksi');
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Admin
+Route::get('/homeAdmin', 'ControllerAdmin@indexAdmin');
+Route::get('/pesanan', 'ControllerAdmin@pesanan');
+Route::get('/katalog', 'ControllerAdmin@katalog');
